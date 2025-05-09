c8c566cc6d3395d7439cc5504c087374f89b53f9
yelin1197 <yelin1197@ewhain.net>
2025-05-09 17:26:15 +0900

    recreate에러 해결

--- src/main/java/umc/study/domain/Review.java
+++ src/main/java/umc/study/domain/Review.java
@@ -23,4 +23,9 @@ public class Review extends BaseEntity {
     @JoinColumn(name = "member_id")
     private Member member;
 
+    // Store와의 관계 추가
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")  // store_id 컬럼에 연결
+    private Store store;  // Store를 참조하는 속성 추가
+
 }
