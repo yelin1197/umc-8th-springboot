a359c6f2518c95a39e25e441b1419416cb61cbb0
yelin1197 <yelin1197@ewhain.net>
2025-05-09 16:18:34 +0900

    6주차 JAP활용

--- src/main/java/umc/study/repository/StoreRepository/StoreRepository.java
+++ src/main/java/umc/study/repository/StoreRepository/StoreRepository.java
@@ -1,4 +1,7 @@
 package umc.study.repository.StoreRepository;
 
-public class StoreRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import umc.study.domain.Store;
+
+public interface StoreRepository extends JpaRepository<Store, Long>, StoreRepositoryCustom {
 }
