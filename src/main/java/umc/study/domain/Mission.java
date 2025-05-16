c0b9065d98b5edaa954232df8d7dfc915efd9040
yelin1197 <yelin1197@ewhain.net>
2025-05-16 12:03:22 +0900

    week7 API 응답 통일, 에러 핸들링

--- src/main/java/umc/study/domain/Mission.java
+++ src/main/java/umc/study/domain/Mission.java
@@ -29,5 +29,6 @@ public class Mission extends BaseEntity {
     private Store store;
 
     @OneToMany(mappedBy = "mission", cascade = CascadeType.ALL)
+    @Builder.Default
     private List<MemberMission> memberMissionList = new ArrayList<>();
 }
